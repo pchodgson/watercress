d059e77bc96a3a1f5afac9d0efed9687a5d291de
peter <pchodgson11@gmail.com>
2019-11-23 17:12:12 -0800

    merged Shalini's changes

--- base.js
+++ base.js
@@ -6,9 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
                                     []);
     related_items_target = document.querySelector('ul.related-items');
     related_items_template = document.querySelector('ul.related-items template');
-    related_items = RenderedElement(related_items_target,
-                                   related_items_target.querySelector('template'),
-                                   items);
     // render(search_result_target, search_result_template, items);
     search_result_target = document.querySelector('tbody.searchResults');
     search_results = RenderedElement(search_result_target,
@@ -17,7 +14,16 @@ document.addEventListener('DOMContentLoaded', () => {
     search_results.render();
 
 });
+function update_comparison_button(event) {
+    console.log(event);
+    if (get_comparison_data().length < 2) { 
+        document.querySelector('button.compare_button').classList.add('hidden_until_two_selected');
 
+    } else {
+        document.querySelector('button.compare_button').classList.remove('hidden_until_two_selected');
+    }
+    
+}
 function Soup(name, price, img, mass, meat, nuts, kcal, co) {
     return {
         name: name,
@@ -80,12 +86,19 @@ function get_comparison_data() {
 function show_comparison_popup(button_click_event) {
     if (get_comparison_data().length < 2) {return;}
     document.querySelector('.popup').style.visibility = "visible";
-    related_items.render();
+    render(related_items_target, related_items_template, items);
+    // render(comparison_table_body_target, comparison_table_body_template, {
+    //     images: get_comparison_data(),
+    //     names: get_comparison_data(),
+    //     nutrition: get_comparison_data(),
+    //     diet: get_comparison_data(),
+    // });
     compare_table.update((data)=>{return {
         images: get_comparison_data(),
         names: get_comparison_data(),
         nutrition: get_comparison_data(),
         diet: get_comparison_data(),
+        cart: get_comparison_data()
     };});
     compare_table.render();
     bold_optimal_attributes();
@@ -115,7 +128,8 @@ function add_to_comparison_items(item_name_drop_event) {
             images: data.images.concat(item),
             names: data.names.concat(item),
             nutrition: data.nutrition.concat(item),
-            diet: data.diet.concat(item)
+            diet: data.diet.concat(item),
+            cart: data.cart.concat(item)
         };
     });
     compare_table.render();
@@ -131,7 +145,8 @@ function remove_from_comparison_items(item_name){
             images: data.images.filter(item_name_filter),
             names: data.names.filter(item_name_filter),
             nutrition: data.nutrition.filter(item_name_filter),
-            diet: data.diet.filter(item_name_filter)
+            diet: data.diet.filter(item_name_filter),
+            cart: data.cart.filter(item_name_filter)
         };
     });
     compare_table.render();
@@ -170,6 +185,67 @@ function add_to_cart_mimicry(onclick){
     quant.innerText = incr.toString();
 }
 
+function render(target, template, data) {
+    function replace(a, b) {
+        a.innerHTML = '';
+        a.appendChild(b);
+    }
+    function construct(template, data) {
+        // var frag = new DocumentFragment();
+        function interpret(template, data) {
+            switch (typeof data) {
+            case 'object':
+                if (Array.isArray(data)) {
+                    return construct_list(template, data);
+                } else {
+                    return construct_object(template, data);
+                }
+            case 'string': return construct_object(template, {'string': data});
+            case 'number': return construct_object(template, {'number': data});
+            default: return data;
+            }
+        }
+        function construct_object(template, data) {
+            var frag = document.importNode(template, true).content.firstElementChild;
+            var chip;
+            for (var key in data) {
+                if (Array.isArray(data[key])){
+                    chip = frag.querySelector('template.' + key);
+                    chip.parentElement.replaceChild(construct_list(chip, data[key]), chip);
+                }
+                switch (key) {
+                case 'img':
+                    chip = frag.querySelector('img');
+                    if (chip) {
+                        chip.src = data[key];
+                    }
+                    break;
+                case 'meat':
+                    chip = frag.querySelector('.' + key);
+                    if (chip) {chip.textContent = (data[key]) ? "Contains Meat" : "Vegetarian";}
+                    break;
+                case 'nuts':
+                    chip = frag.querySelector('.' + key);
+                    if (chip) {chip.textContent = (data[key]) ? "Contains Nuts" : "Nut Free";}
+                    break;
+                default:
+                    chip = frag.querySelector('.' + key);
+                    if (chip) {chip.textContent = data[key];}
+                }
+            }
+            return frag;
+        }
+        function construct_list(template, data) {
+            var frag = new DocumentFragment();
+            for (let i = 0; i < data.length; i++) {
+                frag.append(construct(template, data[i]));
+            };
+            return document.importNode(frag, true);
+        }
+        return interpret(template, data);
+}
+    replace(target, construct(template,data));
+}
 function RenderedElement(target, template, data) {
     function interpret(template, data) {
         switch (typeof data) {
@@ -240,5 +316,5 @@ function RenderedElement(target, template, data) {
         update: update
     };
 }
-// TODO make commands chainable, e.g. element.update().render();
+// TODO make commands chainable, e.g. update().render();
 
